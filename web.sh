87d3911019e1789f0a16a656d5c6fc74477cb408
Ronny Wang <ronnywang@gmail.com>
2016-06-03 22:28:16 +0800

    add PATH for heroku

--- web.sh
+++ web.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+PATH=/app/.heroku/php/bin:/app/.heroku/php/sbin:/app/.heroku/php/bin:/app/.heroku/php/sbin:/app/.heroku/php/bin:/app/.heroku/php/sbin:/app/.heroku/php/bin:/usr/local/bin:/usr/bin:/bin:/app/vendor/bin
 php worker.php &
 php -S 0:$PORT index.php
